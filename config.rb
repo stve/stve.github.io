be0673f807f8c5806d910b96d0ea402fb6eb8b39
Steve Agalloco <steve.agalloco@gmail.com>
2011-12-05 21:19:12 -0500

    css -> stylesheets

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "js"
+javascripts_dir = "javascripts"
 
 output_style = :compressed
