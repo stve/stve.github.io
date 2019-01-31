d05ef25089587ea4290f05dad65de4f14a76ba92
Steve Agalloco <steve.agalloco@gmail.com>
2019-01-31 10:18:50 -0500

    Remove tests

--- Gemfile
+++ Gemfile
@@ -3,9 +3,3 @@ source 'https://rubygems.org'
 group :development do
   gem 'github-pages'
 end
-
-group :test do
-  gem 'rspec'
-  gem 'capybara'
-  gem 'poltergeist'
-end
--- spec/static_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature 'Static Pages' do
-  scenario "resolves the homepage" do
-    visit 'index.html'
-    expect(page).to have_content 'flat'
-  end
-end
